f1bd0aed3136a4bfdd450764852f6796230a5e6c
Akhil Waghmare <akhilwaghmare@gmail.com>
2023-04-15 09:18:01 -0700

    Complete project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,81 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import Section from "../components/Section";
 
 const Home: NextPage = () => {
+  const [theme, setTheme] = useState("light");
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className={`${theme}`}>
       <Head>
-        <title>Create Next App</title>
+        <title>Scrolling dark mode transition</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <Section theme="light" setTheme={setTheme}>
+        <div className="flex-1 flex flex-col justify-center lg:max-w-6xl lg:mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-[2fr_3fr] gap-8">
+            <div className="flex flex-col gap-8 justify-center">
+              <h1 className="text-5xl xl:text-7xl font-bold text-black dark:text-white">
+                Magic Keyboard. Apple Pencil. Endless possibilities.
+              </h1>
+              <p className="text-lg font-semibold text-zinc-400">
+                <span className="text-black dark:text-white">
+                  Pro accessories.
+                </span>{" "}
+                Apple Pencil, Magic Keyboard, and the Smart Keyboard Folio open
+                up even more ways to use iPad Pro. Draw a masterpiece, take
+                notes, or knock out a business plan. These versatile accessories
+                are designed to take your work and creativity to the next level.
+              </p>
+            </div>
+            <div>
+              <div className="relative w-full h-[40rem]">
+                <Image
+                  src="/assets/section1_img.png"
+                  alt="Wi-Fi 6E and 5G"
+                  fill
+                  style={{ objectFit: "contain" }}
+                />
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      </Section>
+      <Section theme="dark" setTheme={setTheme}>
+        <div className="flex-1 flex flex-col justify-center lg:max-w-6xl lg:mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-[2fr_3fr] gap-8">
+            <div className="flex flex-col gap-8 justify-center">
+              <h1 className="text-5xl xl:text-7xl font-bold text-black dark:text-white">
+                Wi-Fi 6E and 5G. Fast friends
+              </h1>
+              <p className="text-lg font-semibold text-zinc-400">
+                <span className="text-black dark:text-white">
+                  Blazing-fast connections.
+                </span>{" "}
+                iPad has always been uniquely portable with superfast Wi-Fi and
+                cellular options. With Wi-Fi 6E, you've got the fastest wireless
+                connection available. So you'll get faster transfers of photos,
+                documents, and large video files.
+              </p>
+            </div>
+            <div>
+              <div className="relative w-full h-[40rem]">
+                <Image
+                  src="/assets/section2_img.jpg"
+                  alt="Wi-Fi 6E and 5G"
+                  fill
+                  style={{ objectFit: "cover", objectPosition: "right" }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
